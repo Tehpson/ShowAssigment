8f3fc8ea3af9b03efb3682379df1a4c15ec071f1
Tehpson <jesper@familjen-pettersson.se>
2022-04-05 17:33:14 +0200

    added DetailedView API call

--- src/views/DetailedView.tsx
+++ src/views/DetailedView.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { ShowAPI } from "../assets/API";
+import { IShow } from "../assets/Types";
+
 export const DetailedView = () => {
+  const [serverResponse, setServerResponse] = useState<any>()
+  const parser = new DOMParser();
+  const [summary, setSummary] = useState<any>()
+
+  useEffect(() => {
+    ShowAPI.get<IShow>(`/shows/${params.id}`)
+  }, [])
+
+  const FetchData = () => {
+
+
+
+    try {
+      if (params.id !== undefined) {
+        const { data } = await API.SearchByID(params.id)
+        setServerResponse(data)
+        console.log(data)
+        setSummary(parser.parseFromString(data.summary, "text/html"))
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div>DetailedView</div>
   )
 }
+function useParams() {
+  throw new Error("Function not implemented.");
+}
+
